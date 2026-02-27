35364e6fd0f1665954ab4aada9785e184d8a67bd
Ashutosh Pradhan <aashutoshpradhan1@gmail.com>
2026-02-27 21:49:35 +0545

    some scroll effects

--- app/pages/Timeline.tsx
+++ app/pages/Timeline.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { ScrollTrigger } from "gsap/all"
+import gsap, { ScrollSmoother, ScrollTrigger } from "gsap/all"
 import AppearIn from "../components/motion/AppearIn"
 import {
     motion,
@@ -12,17 +12,48 @@ import { useEffect, useRef } from "react"
 
 export default function Timeline({id}: {id: number}) {
     useEffect(() => {
-        ScrollTrigger.create({
-            trigger: `#timeline-head`,
-            start: "top top",
-            pin: true,            
-            end: "+=8000px",
-        })
+
+
+
+        if (document.getElementById("portfolio")) {
+        const horizontalSections = gsap.utils.toArray(".horiz-gallery-wrapper");
+
+        horizontalSections.forEach(function (sec, i) {
+            const pinWrap = sec.querySelector(".horiz-gallery-strip");
+
+            let pinWrapWidth;
+            let horizontalScrollLength;
+
+
+            function refresh() {
+                pinWrapWidth = pinWrap.scrollWidth + innerWidth/5 ;
+                horizontalScrollLength = pinWrapWidth - innerWidth ;
+            }
+
+            refresh();
+            // Pinning and horizontal scr olling
+            gsap.to(pinWrap, {
+            scrollTrigger: {
+                scrub: true,
+                trigger: sec,
+                pin: sec,
+                start: "center center",
+                end: () => `+=${pinWrapWidth }`,
+                invalidateOnRefresh: true
+            },
+            x: () => -horizontalScrollLength ,
+            ease: "none"
+            });
+
+            ScrollTrigger.addEventListener("refreshInit", refresh);
+        });
+        }
+
     })
     return (
-        <>
-            <motion.div id="timeline-head" className=" w-full h-screen border-t border-gray-700 bg-[#0a0a0a] z-1 px-5 flex justify-center items-center">
-                <motion.div className="  md:p-40 md:py-40 p-30 px-20 z-1 text-center justify-center items-center flex flex-col self-center" >
+        <div className="flex flex-col w-full ">
+            <motion.div  className="w-full h-screen border-t border-gray-700 bg-[#0a0a0a] z-1 px-5 flex justify-center items-center">
+                <motion.div id="timeline-head" className=" w-full!  md:p-40 md:py-40 p-30 px-20 z-1 text-center justify-center items-center flex flex-col self-center" >
                    <AppearIn>
                         <motion.div className="checkitoutbg  md:p-40 md:py-40 p-30  z-50 text-center justify-center items-center flex flex-col self-center" >
                             <h1 className="text-6xl tracking-tight font-elegant text-center justify-center items-center flex flex-col">
@@ -31,114 +62,97 @@ export default function Timeline({id}: {id: number}) {
                         </motion.div>
                     </AppearIn> 
                 </motion.div>
+            
             </motion.div>
-            <Parallax/>
-        </>
-    )
-}
 
+            <section id="portfolio" >
+                <div className="container-fluid">
+                    <div className="horiz-gallery-wrapper">
+                    <div className="horiz-gallery-strip">
+                        <HorizontalTimeline />
+                    </div>
+                    </div>
 
-function useParallax(value: MotionValue<number>, distance: number) {
-    return useTransform(value, [0, 1], [-distance, distance])
-}
-
-function Image({ title, date, description }: { title:string, date:string, description:string }) {
-    const ref = useRef(null)
-    const { scrollYProgress } = useScroll({ target: ref })
-    const y = useParallax(scrollYProgress, 150)
-
-    return (
-        <section className="img-container">
-            <div ref={ref}>
-                <img
-                    src={`/photos/cityscape/${date}.jpg`}
-                    alt="A London skyscraper"
-                />
-            </div>
-            <motion.h2
-                // Hide until scroll progress is measured
-                initial={{ visibility: "hidden" }}
-                animate={{ visibility: "visible" }}
-                style={{ y }}
-            >{`#${date}`}</motion.h2>
-        </section>
-    )
-}
-
-function Parallax() {
-
-    return (
-        <div className="grid md:grid-cols-2  z-1 projectbg w-full  ">
-            {events.map((item, ind) => {
-  
-                return ind%2 === 0 ? (
-                    <Image key={item.date}  title={item.title} description={item.description} date={item.date} />
-                ) : (
-                    <Image key={item.date} title={item.title} description={item.description} date={item.date} />
-                )
-                
-            })}
+                </div>
+            </section>
 
             <StyleSheet />
+
         </div>
     )
 }
 
 
-function StyleSheet() {
+function StyleSheet(){
     return (
-        <style>{`
-
-        .img-container {
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            scroll-snap-align: start;
-
-            align-items: center;
-            position: relative;
-        }
-
-        .img-container > div {
-            width: 300px;
-            height: 400px;
-            margin: 20px;
-            background: #f5f5f5;
-            overflow: hidden;
-        }
-
-        .img-container img {
-            width: 300px;
-            height: 400px;
-        }
-
-        @media (max-width: 500px) {
-            .img-container > div {
-                width: 150px;
-                height: 200px;
+        <style>
+            {`
+            .container-fluid {
+                width: 100%;
+                padding-right: 0;
+                padding-left: 0;
+                margin-right: auto;
+                margin-left: auto;
             }
 
-            .img-container img {
-                width: 150px;
-                height: 200px;
+            .horiz-gallery-strip,
+            .horiz-gallery-wrapper {
+                display: flex;
+                width: 100%;
+                flex-wrap: nowrap;
+                will-change: transform;
+                position: relative;
             }
-        }
 
-        .img-container h2 {
-            color: #8df0cc;
-            margin: 0;
-            font-family: "Azeret Mono", monospace;
-            font-size: 50px;
-            font-weight: 700;
-            letter-spacing: -3px;
-            line-height: 1.2;
-            position: absolute;
-            display: inline-block;
-            top: calc(50% - 25px);
-            left: calc(50% + 120px);
-        }
 
-    `}</style>
+        }`}
+            
+        </style>
+    )
+}
+
+function TimelineCard({
+    title,
+    date,
+    description,
+}: {
+    title: string
+    date: string
+    description: string
+}) {
+    return (
+        <div className=" w-[400px] h-[200px] aspect-square  rounded-3xl bg-white/5 backdrop-blur-xl border border-white/10 px-10 py-5 flex flex-col justify-between hover:border-white/30">
+
+            <div className="text-sm text-gray-400 tracking-widest uppercase">
+                {date}
+            </div>
+
+            <div>
+                <h2 className="text-3xl font-light text-white mb-4 tracking-tight">
+                    {title}
+                </h2>
+                <p className="text-gray-300 leading-relaxed">
+                    {description}
+                </p>
+            </div>
+
+            <div className="w-full h-[1px] bg-gradient-to-r from-transparent via-white/30 to-transparent" />
+        </div>
+    )
+}
+
+
+
+function HorizontalTimeline() {
+
+    return (
+            <div
+                className="flex items-center w-fit  project-wrap bg-[#0a0a0a] h-fit gap-10  pb-20"
+            >
+                {events.map((event) => (
+                    <TimelineCard key={event.date} {...event} />
+                ))}
+            </div>
     )
 }
 
@@ -166,6 +180,26 @@ const events = [
     date: "Aug 20 2020",
     description: "Welcomed new team members.",
   },
+    {
+    title: "Office Inauguration Function",
+    date: "Oct 01 2029",
+    description: "Started journey with new office",
+  },
+  {
+    title: "Reached New Record",
+    date: "Sept 16 2027",
+    description: "Life time accreditation from IAO",
+  },
+  {
+    title: "Design Meetup",
+    date: "Sept 12 2026",
+    description: "A creative design gathering event.",
+  },
+  {
+    title: "New People Joined",
+    date: "Aug 20 20221",
+    description: "Welcomed new team members.",
+  }
 ]
 
 
